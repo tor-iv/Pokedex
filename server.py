6fb954bfe0d164578e82f9749000997f2aba028c
Yaseen Mohamed <ymohamed@nd.edu>
2021-07-19 04:23:15 +0000

    Update server.py

--- server.py
+++ server.py
@@ -12,14 +12,14 @@ def start_service():
 	resetController = ResetController(pdb=pdb)
 	typeController = typeController(pdb=pdb)
 
-	dispatcher.connect('pokemon_get', '/pokemon/:pokemon_id', controller=pokemonController, action = 'GET_KEY', conditions=dict(method=['GET']))
-	dispatcher.connect('pokemon_put', '/pokemon/:pokemon_id', controller=pokemonController, action = 'PUT_KEY', conditions=dict(method=['PUT']))
-	dispatcher.connect('pokemon_delete', '/pokemon/:pokemon_id', controller=pokemonController, action = 'DELETE_KEY', conditions=dict(method=['DELETE']))
+	dispatcher.connect('pokemon_get', '/pokemon/:pokemon_name', controller=pokemonController, action = 'GET_KEY', conditions=dict(method=['GET']))
+	dispatcher.connect('pokemon_put', '/pokemon/:pokemon_name', controller=pokemonController, action = 'PUT_KEY', conditions=dict(method=['PUT']))
+	dispatcher.connect('pokemon_delete', '/pokemon/:pokemon_name', controller=pokemonController, action = 'DELETE_KEY', conditions=dict(method=['DELETE']))
 	dispatcher.connect('pokemon_index_get', '/pokemon/', controller=pokemonController, action = 'GET_INDEX', conditions=dict(method=['GET']))
 	dispatcher.connect('pokemon_index_post', '/pokemon/', controller=pokemonController, action = 'POST_INDEX', conditions=dict(method=['POST']))
 	dispatcher.connect('pokemon_index_delete', '/pokemon/', controller=pokemonController, action = 'DELETE_INDEX', conditions=dict(method=['DELETE']))
 
-	dispatcher.connect('reset_put', '/reset/:movie_id', controller=resetController, action = 'PUT_KEY', conditions=dict(method=['PUT']))
+	dispatcher.connect('reset_put', '/reset/:pokemon_name', controller=resetController, action = 'PUT_KEY', conditions=dict(method=['PUT']))
 	dispatcher.connect('reset_index_put', '/reset/', controller=resetController, action = 'PUT_INDEX', conditions=dict(method=['PUT']))
 
 
